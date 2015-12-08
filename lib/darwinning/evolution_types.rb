8e3b01286434f013ab048ca5a854695ebdd58177
Brice Videau <brice.videau@imag.fr>
2015-12-08 16:00:29 +0100

    Added MutatuiveReproduction to the list of evolution types.

--- lib/darwinning/evolution_types.rb
+++ lib/darwinning/evolution_types.rb
@@ -2,5 +2,6 @@ module Darwinning
   module EvolutionTypes
     autoload :Mutation,     'darwinning/evolution_types/mutation'
     autoload :Reproduction, 'darwinning/evolution_types/reproduction'
+    autoload :MutativeReproduction, 'darwinning/evolution_types/mutative_reproduction'
   end
 end
