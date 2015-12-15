f5d75a682ecefefb5a3f948f7e9dd34c18afde60
Brice Videau <brice.videau@imag.fr>
2015-12-15 17:26:47 +0100

    Autoload was not used.

--- lib/darwinning.rb
+++ lib/darwinning.rb
@@ -1,9 +1,7 @@
 require_relative 'darwinning/gene'
 require_relative 'darwinning/organism'
-require_relative 'darwinning/evolution_types/re_express'
-require_relative 'darwinning/evolution_types/mutation'
-require_relative 'darwinning/evolution_types/reproduction'
-require_relative 'darwinning/evolution_types/mutative_reproduction'
+require_relative 'darwinning/evolution_types'
+require_relative 'darwinning/selection_types'
 require_relative 'darwinning/population'
 require_relative 'darwinning/config'
 
--- lib/darwinning/evolution_types.rb
+++ lib/darwinning/evolution_types.rb
@@ -1,5 +1,6 @@
 module Darwinning
   module EvolutionTypes
+    autoload :ReExpress,     'darwinning/evolution_types/re_express'
     autoload :Mutation,     'darwinning/evolution_types/mutation'
     autoload :Reproduction, 'darwinning/evolution_types/reproduction'
     autoload :MutativeReproduction, 'darwinning/evolution_types/mutative_reproduction'
